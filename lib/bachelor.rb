05850e9fee845b483af02ac192f609c3a9681e3b
Sahir Nambiar <sahirnambiar@gmail.com>
2019-01-28 05:56:55 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -9,13 +9,18 @@ data.values.flatten.find {|person| person["occupation"] == occupation}["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  data.values.flatten.map {|person| person["hometown"] == hometown ? 1:0}.reduce(:+)
+  #as before, puts every value into an array, then changes array
+  #so that for each repeated hometown key, we turn instances of that
+  #hometown into 1, then sum them all using .reduce(:+), which
+  #works because all those that aren't the hometown argument are zero.
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.values.flatten.find {|person| person["hometown"] == hometown}["occupation"]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = data[season].map {|person| person["age"].to_f}
+  (ages.reduce(:+)/ages.size).round
 end

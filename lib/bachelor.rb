9c46e373934efb29305c5786f2dcb7ba281dfff0
Sahir Nambiar <sahirnambiar@gmail.com>
2019-01-28 05:38:12 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,9 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.fetch(season).find { |person| person["status"] == "Winner" }["name"].split.first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+
 end
 
 def count_contestants_by_hometown(data, hometown)

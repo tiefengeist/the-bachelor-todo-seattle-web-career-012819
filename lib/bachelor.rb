577fe18f80e72a94e3ee2d4549c6f8249e37a7b0
Sahir Nambiar <sahirnambiar@gmail.com>
2019-01-28 05:57:08 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,26 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.fetch(season).find { |person| person["status"] == "Winner" }["name"].split.first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+data.values.flatten.find {|person| person["occupation"] == occupation}["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  data.values.flatten.map {|person| person["hometown"] == hometown ? 1:0}.reduce(:+)
+  #as before, puts every value into an array, then changes array
+  #so that for each repeated hometown key, we turn instances of that
+  #hometown into 1, then sum them all using .reduce(:+), which
+  #works because all those that aren't the hometown argument are zero.
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.values.flatten.find {|person| person["hometown"] == hometown}["occupation"]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = data[season].map {|person| person["age"].to_f}
+  (ages.reduce(:+)/ages.size).round
 end
